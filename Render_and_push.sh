3dea4ca97e31dab98814478387ec226e05f7b731
Oliver Pain <k1806347@login3.pri.rosalind2.alces.network>
2020-07-20 10:34:27 +0100

    Update

---
+++ Render_and_push.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#$ -cwd
+
+
+module add utilities/use.dev
+module add apps/rstudio/3.6.2-singularity
+
+Rscript -e "rmarkdown::render_site()"
+git add .
+git add -u
+git status
+git commit -m 'Update'
+git push
